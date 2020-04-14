593eb9ae0e60f626519a4244564dd125e5fdc893
Gustavo Kumasawa <gustavokumasawa@gmail.com>
2020-04-14 11:17:49 -0300

    Desafio concluído

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, element) => {
+      if (element.type === 'income') {
+        return total + element.value;
+      }
+      return total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, element) => {
+      if (element.type === 'outcome') {
+        return total + element.value;
+      }
+      return total;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
